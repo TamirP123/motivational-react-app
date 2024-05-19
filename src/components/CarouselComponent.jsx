9f7d07090a54b62144500450ba0740f0cd575341
Tamir <tamirphillips@live.com>
2024-05-18 22:45:39 -0400

    Added row of images.

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -1,54 +1,12 @@
 import {Carousel, Navbar, Nav} from 'react-bootstrap';
+import NavbarComponent from './NavbarComponent';
 
 const CarouselComponent = () => {
 
 	return (
     <div className="carousel-container">
       
-      <nav id="navbar" className="navbar navbar-expand-lg navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand mt-1 fs-2 p-2 title" href="#">
-          Stay Motivated.
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-            <li className="nav-item mx-5">
-              <a className="nav-link active" aria-current="page" href="#">
-                Motivation
-              </a>
-            </li>
-            <li className="nav-item mx-5">
-              <a className="nav-link active" aria-current="page" href="#">
-                Self Improvement
-              </a>
-            </li>
-            <li className="nav-item mx-5">
-              <a className="nav-link active" aria-current="page" href="#">
-                Success Story
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Sign up
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      <NavbarComponent/>
 
       <Carousel>
         <Carousel.Item>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,61 @@ import CarouselComponent from "../components/CarouselComponent";
 const HomePage = () => {
 
 	return (
+		<>
 		<CarouselComponent/>
+		<div className="container justify-content-center">
+      <div className="row imageRow justify-content-center mb-5 mt-5 mx-5 px-5">
+        <div className="col-3">
+          <div className="image-wrapper">
+            <img
+              src="https://t3.ftcdn.net/jpg/04/31/55/92/360_F_431559277_rkkDdPgYlypnPwf4EoDIlvkVDiWNBBft.jpg"
+              alt="Image 1"
+              className="img-fluid"
+            />
+            <div className="overlay">
+              <div className="overlay-text">Discipline</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="image-wrapper">
+            <img
+              src="https://i.pinimg.com/736x/98/20/ac/9820ac7eac025766a24d7d767dd3c19a.jpg"
+              alt="Image 2"
+              className="img-fluid"
+            />
+            <div className="overlay">
+              <div className="overlay-text">Faith</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="image-wrapper">
+            <img
+              src="https://miro.medium.com/v2/resize:fit:700/1*yyYSGcw1-HxOlnL8TSwVvQ.jpeg"
+              alt="Image 3"
+              className="img-fluid"
+            />
+            <div className="overlay">
+              <div className="overlay-text">Growth</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="image-wrapper">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGMGcoXZbovpV0DFiwAO7ZbkKQE9faMp3GJkrvxmFewDUHKB8cjcY-mBDf6QSzzTvFaeo&usqp=CAU"
+              alt="Image 4"
+              className="img-fluid"
+            />
+            <div className="overlay">
+              <div className="overlay-text">Confidence</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+	</>
 	);
 };
 
