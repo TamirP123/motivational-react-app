9cb6a16919aa00a3683e50e3374704013b5cf3cf
Tamir <tamirphillips@live.com>
2024-09-09 21:03:37 -0400

    Heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: '../../.env' });
+}
 const express = require("express");
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
